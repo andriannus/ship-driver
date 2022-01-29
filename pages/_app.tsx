a05c3a20dc8fbe810fc4262075581ce502d31203
Andriannus Parasian <andriannus.parasian@qoala.id>
2022-01-29 18:11:09 +0700

    chore: install and setup `fontawesome`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import type { AppProps } from "next/app";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBars,
+  faCalendarAlt,
+  faEllipsisH,
+  faHome,
+  faPlus,
+  faSearch,
+  faUserCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "@/styles/globals.scss";
+
+library.add(
+  faBars,
+  faCalendarAlt,
+  faEllipsisH,
+  faHome,
+  faPlus,
+  faSearch,
+  faUserCircle,
+);
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
