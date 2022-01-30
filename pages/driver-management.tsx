06da08e4899e25ab69ed64ae258f5f3a17f4761e
Andriannus Parasian <andriannus.parasian@qoala.id>
2022-01-30 11:03:10 +0700

    improvement: add search and add button

--- pages/driver-management.tsx
+++ pages/driver-management.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { Banner } from "@/components/banner";
+import { Button } from "@/components/button";
 import { DefaultLayout } from "@/layouts/default";
+
+import driverStyles from "@/styles/driver-management.module.scss";
 
 const DriverManagement: NextPage = () => {
   return (
@@ -17,7 +21,30 @@ const DriverManagement: NextPage = () => {
         <Banner
           title="Dashboard Management"
           subtitle="Data driver you work with."
-        />
+        >
+          <div className={driverStyles["Driver-actions"]}>
+            <div className={driverStyles["Search"]}>
+              <FontAwesomeIcon
+                className={driverStyles["Search-icon"]}
+                icon="search"
+              />
+
+              <div className={driverStyles["Search-field"]}>
+                <input
+                  aria-label="Cari Driver"
+                  className={driverStyles["Search-input"]}
+                  placeholder="Cari Driver"
+                  title="Cari Driver"
+                  type="text"
+                />
+              </div>
+            </div>
+
+            <Button color="primary" icon="plus">
+              <span>TAMBAH DRIVER</span>
+            </Button>
+          </div>
+        </Banner>
       </>
     </DefaultLayout>
   );
