de9d671d6de39accc46a382e76c3e6b3376afd6b
Andriannus Parasian <andriannus.parasian@qoala.id>
2022-01-31 20:02:44 +0700

    improvement: implement bottom sheet

--- components/app-bar/app-bar.component.tsx
+++ components/app-bar/app-bar.component.tsx
@@ -1,32 +1,65 @@
-import { FC } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { FC, useState } from "react";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import appBarStyles from "./app-bar.module.scss";
 
+import { BottomSheet } from "@/components/bottom-sheet";
+import { SIDEBAR_MENUS } from "@/constants/menu.constant";
+import { getValidClassNames } from "@/utils/transform";
+
 const AppBar: FC<{}> = () => {
+  const router = useRouter();
+
+  const [isBottomSheetShown, setBottomSheetStatus] = useState(false);
+
   return (
-    <header className={appBarStyles["AppBar"]}>
-      <button className={appBarStyles["AppBar-bars"]}>
-        <FontAwesomeIcon icon="bars" size="lg" />
-      </button>
+    <>
+      <header className={appBarStyles["AppBar"]}>
+        <button
+          className={appBarStyles["AppBar-bars"]}
+          onClick={() => setBottomSheetStatus(true)}
+        >
+          <FontAwesomeIcon icon="bars" size="lg" />
+        </button>
 
-      <div className={appBarStyles["AppBar-brand"]}>
-        <img src="/images/logo.png" alt="Logo Shipper" />
-        <span className={appBarStyles["AppBar-brandText"]}>Shipper</span>
-      </div>
+        <div className={appBarStyles["AppBar-brand"]}>
+          <img src="/images/logo.png" alt="Logo Shipper" />
+          <span className={appBarStyles["AppBar-brandText"]}>Shipper</span>
+        </div>
 
-      <div className={appBarStyles["AppBar-action"]}>
-        <span className={appBarStyles["AppBar-actionText"]}>
-          Hello, <span className="text-ruby-500">Shipper User</span>
-        </span>
+        <div className={appBarStyles["AppBar-action"]}>
+          <span className={appBarStyles["AppBar-actionText"]}>
+            Hello, <span className="text-ruby-500">Shipper User</span>
+          </span>
 
-        <FontAwesomeIcon
-          className="ml-sm text-gray-400"
-          icon="user-circle"
-          size="2x"
-        />
-      </div>
-    </header>
+          <FontAwesomeIcon
+            className="ml-sm text-gray-400"
+            icon="user-circle"
+            size="2x"
+          />
+        </div>
+      </header>
+
+      <BottomSheet value={isBottomSheetShown} onChange={setBottomSheetStatus}>
+        <ul>
+          {SIDEBAR_MENUS.map((menu, index) => {
+            return (
+              <li key={index} className="Menu">
+                <Link href={menu.href}>
+                  <a className={getValidClassNames(router.pathname, menu.href)}>
+                    <FontAwesomeIcon icon={menu.icon as IconProp} />
+                    <span>{menu.text}</span>
+                  </a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </BottomSheet>
+    </>
   );
 };
 
